8d79ac4e24f8e511ed0a89dd2c1df1e44223ad23
Masahiro Aoki <rafe.kun@gmail.com>
2022-10-28 13:25:22 +0900

    update ui

--- app/src/main/java/com/example/weatherkitandroidm3/MainActivity.kt
+++ app/src/main/java/com/example/weatherkitandroidm3/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.weatherkitandroidm3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // データを用意
+        val data = listOf(
+            mapOf("name" to "沖縄", "detail" to "晴れ"),
+            mapOf("name" to "大阪", "detail" to "曇り"),
+            mapOf("name" to "東京", "detail" to "雨"),
+        )
+
+        val list = findViewById<ListView>(R.id.list)
+        list.adapter = SimpleAdapter(
+            this,
+            data,
+            android.R.layout.simple_list_item_2,
+            arrayOf("name", "detail"),
+            intArrayOf(android.R.id.text1, android.R.id.text2)
+        )
     }
 }
